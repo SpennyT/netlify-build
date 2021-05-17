749bc07d0f4883f386ce08999bdb05b5c75fffa7
Spencer Thompson <spencer.thompson@mail.mcgill.ca>
2021-05-17 11:53:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Deploying w netlify</h1>
+      <p> a;sdlkfjas d;lfjasd;lfkjasd;lfkajsd;lfkaje;oifajs;dlfknsoeinsdf;lkgjsdroigjzf;gklj gsdf g;lskfjgopsirj gs;lkfjg ada;dlivhae fajxdopv ;ldkjf apoeif  a;slkfj adlf;ajdfewelvnhowkdf osdfle lsieh </p>
+    </div>
+  );
+}
+
+export default App;
