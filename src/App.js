5673b44edd97119f9df7b7e52983e41c5d9c91d6
Spencer Thompson <spencer.thompson@mail.mcgill.ca>
2021-05-17 14:11:30 -0400

    2nd header test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Deploying w netlify</h1>
       <p> a;sdlkfjas d;lfjasd;lfkjasd;lfkajsd;lfkaje;oifajs;dlfknsoeinsdf;lkgjsdroigjzf;gklj gsdf g;lskfjgopsirj gs;lkfjg ada;dlivhae fajxdopv ;ldkjf apoeif  a;slkfj adlf;ajdfewelvnhowkdf osdfle lsieh </p>
+      <h2>test TEST</h2>
     </div>
   );
 }
