9608a2beccc1a89fda9bd783ea69f6783bca1e0f
Spencer Thompson <spencer.thompson@mail.mcgill.ca>
2021-05-17 14:57:24 -0400

    backgrounds

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Deploying w netlify</h1>
       <p> a;sdlkfjas d;lfjasd;lfkjasd;lfkajsd;lfkaje;oifajs;dlfknsoeinsdf;lkgjsdroigjzf;gklj gsdf g;lskfjgopsirj gs;lkfjg ada;dlivhae fajxdopv ;ldkjf apoeif  a;slkfj adlf;ajdfewelvnhowkdf osdfle lsieh </p>
       <h2>test TEST</h2>
+      
     </div>
   );
 }
